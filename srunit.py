89e6b1aa3db1a49b8d0c7d2d3b15be10ff49ba0b
21jun <21jun7654@gmail.com>
2023-04-04 15:09:05 +0900

    add setup tool

--- srunit.py
+++ srunit.py
@@ -5,52 +5,74 @@ import shutil
 from string import Template
 
 
-with open("cache/default.conf", "r") as f:
-    f.read()
-
-parser = argparse.ArgumentParser(description="Argparse Tutorial")
-parser.add_argument("--dry_run", "-d", action="store_true")
-parser.add_argument("--job_name", "-j", default=None)
-parser.add_argument("--output_path", "-o", default=None)
-parser.add_argument("--gpu_type", "-t", default="A100-80GB")
-parser.add_argument("--gpu_num", "-n", default="8")
-parser.add_argument("--cpu_num", "-c", default="16")
-parser.add_argument("--script_path", "-f", default="finetune.sh")
-parser.add_argument("--add", "-a", default="")
-args = parser.parse_args()
 
 
-JOB_NAME = args.job_name
-OUTPUT_PATH = args.output_path
-GPU_TYPE = args.gpu_type
-GPU_NUM = args.gpu_num
-CPU_NUM = args.cpu_num
-SCRIPT_PATH = args.script_path
-CHECKPOINTS_PATH = "checkpoints"
-ADDITIONAL_ARGS = args.add
-dry_run = args.dry_run
+def main():
 
-# Additional
-
-
-if __name__ == "__main__":
-
-    # Check configuration files (this will overwrite default.conf)
-    # find there is .conf file under current directory with .srunit directory and if exists
-    # set value `custom_config` to True
+    # check if there is a.conf file under .srunit directory
+    # if exists, set value `custom_config` to True
     custom_config = False
-    for file in os.listdir("."):
-        if file.endswith(".conf") and ".srunit" in file:
-            custom_config = True
-            with open(file, "r") as f:
-                f.read()
-            break
+    custom_config_dir = Path(".srunit")
+    if custom_config_dir.is_dir():
+        custom_config = True
+        for file in list(custom_config_dir.glob('**/*')):
+            if file.suffix == ".conf":
+                with open(file, "r") as f:
+                    values = f.read()
+                break
 
+    # print(custom_config)
+    if custom_config:
+        print("Custom config file found")
+    else:
+        print("No custom config file found")
     # if there is no custom config file, use default.conf
     if not custom_config:
         # read cache/default.conf and save its value to `values`
         with open("cache/default.conf", "r") as f:
             values = f.read()
+
+    for value in values.split("\n"):
+        print(value)
+
+    defaults ={}
+    for value in values.split("\n"):
+        if len(value) == 0:
+            continue
+        key, val = value.split("=")
+        # can we save it into dictionary?
+        if val.isdigit():
+            val = int(val)
+        elif val == "":
+            val = None
+        defaults[key] = val
+
+    # print(defaults)
+
+
+    parser = argparse.ArgumentParser(description="Argparse Tutorial")
+    parser.add_argument("--dry_run", "-d", action="store_true")
+
+    for key, val in defaults.items():
+        arg_key = "--" + key.lower().replace("-","_")
+        print(arg_key)
+        parser.add_argument(arg_key, default=val)
+
+    args = parser.parse_args()
+
+    print(args)
+
+    JOB_NAME = args.job_name
+    OUTPUT_PATH = args.output_path
+    GPU_TYPE = args.gpu_type
+    GPU_NUM = args.gpu_num
+    CPU_NUM = args.cpu_num
+    SCRIPT_PATH = args.script_path
+    CHECKPOINTS_PATH = "checkpoints"
+    ADDITIONAL_ARGS = ""
+    DRY_RUN = args.dry_run
+
+    # Additional
 
     exp_root = Path(SCRIPT_PATH).parent
     RUN_ROOT_PATH = exp_root / "runs"
@@ -61,15 +83,15 @@ if __name__ == "__main__":
     # get output_path by script_path
 
     RUN_PATH = RUN_ROOT_PATH / f"run_{i}"
-    if not dry_run:
+    if not DRY_RUN:
         Path.mkdir(RUN_PATH, exist_ok=True, parents=True)
 
     OUTPUT_PATH = RUN_PATH / "out"
-    if not dry_run:
+    if not DRY_RUN:
         Path.mkdir(OUTPUT_PATH, exist_ok=True, parents=True)
 
     RUN_SCRIPT_PATH = RUN_PATH / "finetune.sh"
-    if not dry_run:
+    if not DRY_RUN:
         shutil.copy2(SCRIPT_PATH, RUN_SCRIPT_PATH)
 
     if JOB_NAME is None:
@@ -96,3 +118,9 @@ if __name__ == "__main__":
         src = Template(f.read())
         result = src.safe_substitute(values)
         print(result)
+
+
+
+if __name__ == "__main__":
+
+   main()
