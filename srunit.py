e3462bd6b69655e0f6ec66e9761beac29b66fae9
21jun <21jun7654@gmail.com>
2023-04-05 14:52:58 +0900

    minor

--- srunit.py
+++ srunit.py
@@ -5,41 +5,59 @@ import shutil
 from string import Template
 
 
-
-
 def main():
 
-    # check if there is a.conf file under .srunit directory
-    # if exists, set value `custom_config` to True
-    custom_config = False
+    # check if there is custom.conf file under .srunit directory
+    custom_config, custom_template = False, False
     custom_config_dir = Path(".srunit")
     if custom_config_dir.is_dir():
-        custom_config = True
-        for file in list(custom_config_dir.glob('**/*')):
+        for file in list(custom_config_dir.glob("**/*")):
             if file.suffix == ".conf":
+                if custom_config:
+                    raise ValueError(
+                        "There are more than one custom config files in .srunit directory"
+                    )
+                custom_config = True
                 with open(file, "r") as f:
-                    values = f.read()
-                break
+                    confs = f.read()
+            if file.suffix == ".sbatch":
+                if custom_template:
+                    raise ValueError(
+                        "There are more than one custom template files in .srunit directory"
+                    )
+                custom_template = True
+                with open(file, "r") as f:
+                    template = f.read()
 
     # print(custom_config)
     if custom_config:
         print("Custom config file found")
     else:
-        print("No custom config file found")
+        print("No custom config file found, using default.conf")
+
+    if custom_template:
+        print("Custom template file found")
+    else:
+        print("No custom template file found, using template.sbatch")
+
     # if there is no custom config file, use default.conf
     if not custom_config:
         # read cache/default.conf and save its value to `values`
         with open("cache/default.conf", "r") as f:
-            values = f.read()
+            confs = f.read()
 
-    for value in values.split("\n"):
-        print(value)
+    if not custom_template:
+        with open("template.sbatch", "r") as f:
+            template = f.read()
 
-    defaults ={}
-    for value in values.split("\n"):
-        if len(value) == 0:
+    for conf in confs.split("\n"):
+        print(conf)
+
+    defaults = {}
+    for conf in confs.split("\n"):
+        if len(conf) == 0:
             continue
-        key, val = value.split("=")
+        key, val = conf.split("=")
         # can we save it into dictionary?
         if val.isdigit():
             val = int(val)
@@ -49,19 +67,23 @@ def main():
 
     # print(defaults)
 
-
-    parser = argparse.ArgumentParser(description="Argparse Tutorial")
+    parser = argparse.ArgumentParser(description="SRUNIT v2.0.0")
     parser.add_argument("--dry_run", "-d", action="store_true")
+    parser.add_argument("--script_path", "-f", default=None)
 
     for key, val in defaults.items():
-        arg_key = "--" + key.lower().replace("-","_")
+        arg_key = "--" + key.lower().replace("-", "_")
         print(arg_key)
-        parser.add_argument(arg_key, default=val)
+        parser.add_argument(
+            arg_key,
+            default=val,
+        )
 
     args = parser.parse_args()
 
     print(args)
 
+    # Essential arguments (required for running the script)
     JOB_NAME = args.job_name
     OUTPUT_PATH = args.output_path
     GPU_TYPE = args.gpu_type
@@ -70,49 +92,53 @@ def main():
     SCRIPT_PATH = args.script_path
     CHECKPOINTS_PATH = "checkpoints"
     ADDITIONAL_ARGS = ""
-    DRY_RUN = args.dry_run
 
-    # Additional
+    DRY_RUN = args.dry_run
 
     exp_root = Path(SCRIPT_PATH).parent
     RUN_ROOT_PATH = exp_root / "runs"
 
+    # get run number (run_0, run_1, ...)
     i = 0
     while os.path.exists(RUN_ROOT_PATH / f"run_{i}"):
         i += 1
     # get output_path by script_path
 
+    # create run directory
     RUN_PATH = RUN_ROOT_PATH / f"run_{i}"
     if not DRY_RUN:
         Path.mkdir(RUN_PATH, exist_ok=True, parents=True)
 
+    # create output directory (under run directory for each run `out`)
     OUTPUT_PATH = RUN_PATH / "out"
     if not DRY_RUN:
         Path.mkdir(OUTPUT_PATH, exist_ok=True, parents=True)
 
+    # copy script to run directory
     RUN_SCRIPT_PATH = RUN_PATH / "finetune.sh"
     if not DRY_RUN:
         shutil.copy2(SCRIPT_PATH, RUN_SCRIPT_PATH)
 
+    # set default job name if not specified (for sbatch)
     if JOB_NAME is None:
         JOB_NAME = exp_root.stem
         JOB_NAME = JOB_NAME + "_" + str(i)
 
     CHECKPOINTS_PATH = RUN_PATH / "checkpoints"
 
-    print()
+    values = {}
 
-    values = {
-        "JOB_NAME": JOB_NAME,
-        "OUTPUT_PATH": OUTPUT_PATH,
-        "GPU_TYPE": GPU_TYPE,
-        "GPU_NUM": GPU_NUM,
-        "CPU_NUM": CPU_NUM,
-        "SCRIPT_PATH": SCRIPT_PATH,
-        "CHECKPOINTS_PATH": CHECKPOINTS_PATH,
-        "RUN_SCRIPT_PATH": RUN_SCRIPT_PATH,
-        "ADDITIONAL_ARGS": ADDITIONAL_ARGS,
-    }
+    # values = {
+    #     "JOB_NAME": JOB_NAME,
+    #     "OUTPUT_PATH": OUTPUT_PATH,
+    #     "GPU_TYPE": GPU_TYPE,
+    #     "GPU_NUM": GPU_NUM,
+    #     "CPU_NUM": CPU_NUM,
+    #     "SCRIPT_PATH": SCRIPT_PATH,
+    #     "CHECKPOINTS_PATH": CHECKPOINTS_PATH,
+    #     "RUN_SCRIPT_PATH": RUN_SCRIPT_PATH,
+    #     "ADDITIONAL_ARGS": ADDITIONAL_ARGS,
+    # }
 
     with open("template.sbatch", "r") as f:
         src = Template(f.read())
@@ -120,7 +146,6 @@ def main():
         print(result)
 
 
-
 if __name__ == "__main__":
 
-   main()
+    main()
